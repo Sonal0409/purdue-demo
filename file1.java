06868b66bfa90c32feeb6a369579c193b672ec0f
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2023-08-26 10:55:36 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -5,3 +5,10 @@ class pogram1{
   system.out.println("hello sonal");
   system.out.println("hello Purdue");
 }
+
+class program2{
+
+  system.out.println("hello github");
+  system.out.println("hello sonal");
+  system.out.println("hello Purdue");
+}
